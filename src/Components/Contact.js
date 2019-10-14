acb7bcb2d62e3f26125860107ccad9d9562b6d4c
LUU <florentluu@gmail.com>
2019-10-14 13:46:03 +0200

    State status REACT

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import './Contact.css';
 
-function Contact(props) {
-    return(
-        <div className="Contact">
-            <img className="avatar" src={props.avatar} alt={props.name} />
-            <div>
-                <h3 className= "name">{props.name}</h3>
-                <div className="status">
-                    <h4 className={props.online ? "status-online" : "status-offline"}></h4>
-                    <h4 className="status-text">{props.online ? "online" : "offline"}</h4>
-                </div>
+class Contact extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            status: false,
+        };
+    }
+    render() {
+        return(
+            <div className="Contact">
+                <img className="avatar" src={this.props.avatar} alt={this.props.name} />
+                <h3 className= "name">{this.props.name}</h3>
+                    <div className="status">
+                        <h4 className={this.props.online ? "status-online" : "status-offline"}></h4>
+                        <h4 className="status-text">{this.props.online ? "online" : "offline"}</h4>
+                        <span className={this.state.favorite ? 'is-favorite' : ''}
+                        onClick={event => {
+                            const newStatus = !this.state.status;
+                            this.setState({ status: newStatus });
+                        }}
+                        >
+                        </span>
+                    </div>
             </div>   
-        </div>
-    );
-}
 
+        )
+    };
+}
 
 export default Contact;
